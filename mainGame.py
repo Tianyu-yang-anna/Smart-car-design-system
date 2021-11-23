e13c7e84cef93fd6b727617c312b9b63a33e1f3e
Anna <94773545+lammmmmbtianyu@users.noreply.github.com>
2021-11-22 21:32:50 -0500

    Add files via upload

---
+++ mainGame.py
@@ -0,0 +1,209 @@
+#coding: utf-8
+
+import pygame
+from pygame.locals import *
+from sys import exit
+import numpy as np
+import copy
+
+import myItem
+import myCarAI
+
+# 绘图
+def mapPaint(pygame, screen, BigCircle, smallCircle):
+    screen.fill((255, 255, 255))
+    # 绘图
+    pygame.draw.circle(screen, BigCircle.rc, BigCircle.rp, BigCircle.rr)
+    pygame.draw.circle(screen, smallCircle.rc, smallCircle.rp, smallCircle.rr)
+    pygame.draw.rect(screen, (0, 0, 255), Rect((0, 1), (30, 158)))
+
+# 绘制小车
+def carPaint(pygame, screen, cars):
+    for i in range(len(cars)):
+        car = cars[i]
+        pygame.draw.rect(screen, (0, 255, 0), Rect((car.x, car.y), (car.size)))
+        pygame.draw.line(screen, (255, 255, 0), (car.x + car.size[0] / 2, car.y + car.size[1] / 2),
+                         (car.dis_pos[0], car.dis_pos[1]))
+        # pygame.draw.line(screen, (0, 255, 0), (car.x + car.size[0] / 2, car.y + car.size[1] / 2),
+        #                  (car.ldis_pos[0], car.ldis_pos[1]))
+        # pygame.draw.line(screen, (0, 255, 0), (car.x + car.size[0] / 2, car.y + car.size[1] / 2),
+        #                  (car.lldis_pos[0], car.lldis_pos[1]))
+        # pygame.draw.line(screen, (0, 255, 0), (car.x + car.size[0] / 2, car.y + car.size[1] / 2),
+        #                  (car.rdis_pos[0], car.rdis_pos[1]))
+        # pygame.draw.line(screen, (0, 255, 0), (car.x + car.size[0] / 2, car.y + car.size[1] / 2),
+        #                  (car.rrdis_pos[0], car.rrdis_pos[1]))
+
+# 选择表现最好的AI
+def pick_best_AI(cars, carAis):
+    bestindex = 0
+    farDistance = 1000
+
+    for i in range(len(cars)):
+        if cars[i].goal == True:
+            farDistance = cars[i].distance
+            bestindex = i
+            print("1")
+            break
+        if cars[i].distance < farDistance:
+            farDistance = cars[i].distance
+            bestindex = i
+        
+    print("cur best car distance: ", cars[0].distance)
+    bestAI = myCarAI.carAI()
+    w, b = carAis[bestindex].get_weights()
+    bestAI.assign_weights(w, b)
+    return bestAI
+
+# 开始训练
+def gameStart():
+    pygame.init()
+    bigCircle = myItem.Circle((0,0,0), (0,320), 320)
+    smallCircle = myItem.Circle((255, 255, 255), (0,320), 160)
+    # 生成carnum辆car和carnum个AI
+    carnum = 100
+    cars = []
+    carAis = []
+    for i in range(carnum):
+        cars.append(myItem.car(2, 560, (15,15)))
+        carAis.append(myCarAI.carAI())
+    # init屏幕
+    screen = pygame.display.set_mode((480, 640), 0, 32)
+    # Clock对象，使小车运动更流畅
+    clock = pygame.time.Clock()
+    # 训练次数
+    n_epoches = 60
+
+    for k in range(n_epoches):
+        print("第%d次----------------------"%k)
+        # 每次的时间为1000
+        for i in range(1000):
+            # 手动操作，还未加入
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    exit()
+            # AI
+            # input car.r  car.v car.dis-s 7 output speed, direction
+            # 根据speed和direction控制小车
+            # 计算移动距离
+            time_passed = clock.tick(); time_passed_seconds = time_passed / 1000.
+            for i in range(len(cars)):
+                car = cars[i]
+                if car.alive == False:
+                    continue
+                car.get_dis_to_wall((0, 320), 320, (0, 320), 160)
+                car.postion_move(time_passed_seconds)
+                carAi = carAis[i]
+                
+                # 输入神经网络的指标( /30为了防止输入过大)
+                data = np.array([car.r/30, car.v/30, car.dis_wall/30, car.ldis_wall/30,
+                                 car.lldis_wall/30, car.rdis_wall/30, car.rrdis_wall/30])
+                # if i == 0:
+                #     print(data)
+                data = data.T
+                # 控制方向
+                v,dir = carAi.forward(data)
+                if v == -1:
+                    car.speed_down()
+                elif v == 1:
+                    car.speed_up()
+                if dir == -1:
+                    car.left_move()
+                elif dir == 1:
+                    car.right_move()
+                # 到达
+                if car.is_goal() == True:
+                    print("goal!")
+                    carAi.save("goal_para/goal_{}".format(str(i)))
+                # 判断is_live
+                car.is_live(smallCircle.rp, smallCircle.rr, bigCircle.rp, bigCircle.rr)
+                car.distance = car.y
+
+            # update
+            mapPaint(pygame, screen, bigCircle, smallCircle)
+            carPaint(pygame, screen,cars)
+            pygame.display.update()
+
+            # 是否结束
+            flag = 0
+            for i in range(len(cars)):
+                car = cars[i]
+                if car.alive == True:
+                    flag = 1
+            if flag == 0: break
+
+        # 选出最好的一个AI
+        bestAI = pick_best_AI(cars, carAis)
+        # 下一轮
+        cars.clear(); carAis.clear()
+        for i in range(carnum):
+            cars.append(myItem.car(2, 560, (15, 15)))
+        carAis = myCarAI.GeneOptimize.gene_algo(bestAI, carnum)
+
+def testAI(filename):
+    pygame.init()
+    bigCircle = myItem.Circle((0, 0, 0), (0, 320), 320)
+    smallCircle = myItem.Circle((255, 255, 255), (0, 320), 160)
+
+    car = myItem.car(2, 560, (15, 15))
+    carAi = myCarAI.carAI()
+    #load训练好的神经网络
+    carAi.load(filename)
+    screen = pygame.display.set_mode((480, 640), 0, 32)
+    # Clock对象
+    clock = pygame.time.Clock()
+    for i in range(5000):
+        # 手动操作
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+        # AI
+        # input car.r  car.v car.dis-s 7 output speed, direction
+        # 根据speed和direction控制小车
+        # 计算移动距离
+        time_passed = clock.tick()
+        time_passed_seconds = time_passed / 1000.
+        car.postion_move(time_passed_seconds)
+        car.get_dis_to_wall((0, 320), 320, (0, 320), 160)
+
+        # 输入神经网络的指标( /30为了防止输入过大)
+        data = np.array([car.r/30, car.v/30, car.dis_wall/30, car.ldis_wall/30,
+                            car.lldis_wall/30, car.rdis_wall/30, car.rrdis_wall/30])
+        data = data.T
+        v, dir = carAi.forward(data)
+        if v == -1:
+            car.speed_down()
+        elif v == 1:
+            car.speed_up()
+        if dir == -1:
+            car.left_move()
+        elif dir == 1:
+            car.right_move()
+        if car.is_goal() == True:
+            print("goal!")
+            break
+        if car.is_live(smallCircle.rp, smallCircle.rr, bigCircle.rp, bigCircle.rr) == False:
+            print("error")
+            break
+
+        mapPaint(pygame, screen, bigCircle, smallCircle)
+        pygame.draw.rect(screen, (0, 255, 0), Rect((car.x, car.y), (car.size)))
+        pygame.draw.line(screen, (255, 255, 0), (car.x + car.size[0] / 2, car.y + car.size[1] / 2),
+                         (car.dis_pos[0], car.dis_pos[1]))
+        pygame.draw.line(screen, (0, 255, 0), (car.x + car.size[0] / 2, car.y + car.size[1] / 2),
+                         (car.ldis_pos[0], car.ldis_pos[1]))
+        pygame.draw.line(screen, (0, 255, 0), (car.x + car.size[0] / 2, car.y + car.size[1] / 2),
+                         (car.lldis_pos[0], car.lldis_pos[1]))
+        pygame.draw.line(screen, (0, 255, 0), (car.x + car.size[0] / 2, car.y + car.size[1] / 2),
+                         (car.rdis_pos[0], car.rdis_pos[1]))
+        pygame.draw.line(screen, (0, 255, 0), (car.x + car.size[0] / 2, car.y + car.size[1] / 2),
+                         (car.rrdis_pos[0], car.rrdis_pos[1]))
+
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    gameStart()
+    # filename = "goal_para/goal_76.npz"
+    # testAI(filename)
+
+
---
+++ myCarAI.py
@@ -0,0 +1,185 @@
+#coding: utf-8
+
+# 遗传算法与神经网络
+import numpy as np
+from copy import deepcopy
+
+# 神经网络
+# 数据层
+class Data:
+    def __init__(self, x):
+        self.x = x
+    def forward(self):
+        # Mini-batch
+        return self.x
+
+# 全连接层
+class FullyConnect:
+    def __init__(self, l_x, l_y, weights = None, bias = None):
+        self.l_x = l_x
+        self.l_y = l_y
+        self.weights = np.random.randn(l_y, l_x) * np.sqrt(2 / l_x)
+        self.bias = np.random.randn(l_y, 1)
+    def forward(self, x):
+        self.y = np.dot(self.weights, x) + self.bias
+        return self.y
+
+# Sigmoid函数
+class Sigmoid:
+    def __init__(self):
+        pass
+    
+    def sigmoid(self, x):
+        return 1/(1+np.exp(-np.maximum(x,-100)))
+
+    def forward(self, x):
+        self.x = x
+        self.y = self.sigmoid(x)
+        return self.y
+
+# 控制小车的AI
+class carAI:
+    def __init__(self,node_list = [7,15,2]):
+        self.fullyconnect_list = []
+        for i in range(len(node_list) - 1):
+            linear = FullyConnect(node_list[i], node_list[i+1])
+            self.fullyconnect_list.append(linear)
+
+    # 调整速度与方向
+    def _adjust(self, t, v1, v2):
+        if t < v1:
+            return -1
+        elif t > v2:
+            return 1
+    # 速度
+    def _car_v_adjust(self, v_flag, v_thre1 = 0.3, v_thre2 = 0.7):
+        car_v = self._adjust(v_flag, v_thre1, v_thre2)
+        return car_v
+    # 方向
+    def _car_dir_adjust(self, dir_flag, dir_thre1 = 0.2, dir_thre2 = 0.8):
+        car_dir = self._adjust(dir_flag, dir_thre1, dir_thre2)
+        return car_dir
+    
+    def forward(self,x):
+        for linear in self.fullyconnect_list:
+            x = linear.forward(x)
+            x = Sigmoid().forward(x)
+        
+        # 方向
+        dir_flag = x[0][0]
+        # 速度
+        v_flag = x[1][0]
+        car_v = self._car_v_adjust(v_flag)
+        car_dir = self._car_dir_adjust(dir_flag)
+
+        return (car_v, car_dir)
+    
+    # 重新赋予权值,与get_weights配对使用
+    def assign_weights(self, weightsList = None, biasList = None):
+        for i in range(len(self.fullyconnect_list)):
+            linear = self.fullyconnect_list[i]
+            linear.weights = deepcopy(weightsList[i])
+            linear.bias = deepcopy(biasList[i])
+
+    # 获得权值
+    def get_weights(self):
+        weightsList = []
+        biasList = []
+
+        for i in range(len(self.fullyconnect_list)):
+            linear = self.fullyconnect_list[i]
+            weightsList.append(linear.weights)
+            biasList.append(linear.bias)
+
+        return weightsList, biasList
+
+    def save(self, filename):
+        np.savez(filename, fullyconnect_list = np.array(self.fullyconnect_list))
+
+    def load(self, filename):
+        para = np.load(filename)
+        self.fullyconnect_list = list(deepcopy(para['fullyconnect_list']))
+
+class GeneOptimize:
+    def __init__(self):
+        pass
+    # 扰动
+    @classmethod
+    def _distur_param(self, weightsList, biasList, k, r = 0.1):
+        carAIs = []
+        for i in range(k):
+            carai = carAI()
+            w = deepcopy(weightsList)
+            b = deepcopy(biasList)
+            for i in range(len(w)):
+                n,m = w[i].shape
+                w[i] += r*np.random.randn(n, m)
+            for i in range(len(b)):
+                n,m = b[i].shape
+                b[i] += r*np.random.randn(n, m)
+            
+            carai.assign_weights(w,b)
+            carAIs.append(carai)
+        return carAIs
+
+    # 替换
+    @classmethod
+    def _replace_one_layer_param(self, weightsList,biasList,k):
+        carAIs = []
+        for i in range(k):
+            carai = carAI()
+            carAIs.append(carai)
+            w = deepcopy(weightsList)
+            b = deepcopy(biasList)
+            for i in range(len(w)):
+                t = np.random.rand(1)[0]
+                if t < 0.3:
+                    n,m = w[i].shape
+                    w[i] += np.random.randn(n, m)
+            for i in range(len(b)):
+                t = np.random.rand(1)[0]
+                if t < 0.7:
+                    n,m = b[i].shape
+                    b[i] += np.random.randn(n, m)
+            carai.assign_weights(w,b)
+            carAIs.append(carai)
+        return carAIs
+
+    # 遗传算法
+    # 输入上次最好的那个
+    @classmethod
+    def gene_algo(self, parcarAI, n):
+        carAIs = []
+        carAIs.append(parcarAI)
+        k1 = n//4; k2 = n - 3*k1-1
+        # 一半权值小幅度扰动(0.1,0.3)
+        # 1/4权值大幅度扰动(0.6)
+        # 1/4随机一个层(使用np.random.randn)
+        weightsList, biasList = parcarAI.get_weights()
+        # 0.1
+        carAIs.extend(self._distur_param(weightsList, biasList, k1, 0.1))
+        # 0.2
+        carAIs.extend(self._distur_param(weightsList, biasList, k1, 0.5))
+        # 0.6
+        carAIs.extend(self._distur_param(weightsList, biasList, k1, 0.8))
+        # 随机扰动一层
+        carAIs.extend(self._replace_one_layer_param(weightsList, biasList, k2))
+        # print("-------", parcarAI.get_weights() == carAIs[0].get_weights())
+        return carAIs
+
+if __name__ == '__main__':
+    carai = carAI()
+    car_ai3 = carAI()
+
+    a = np.random.randn(7,1)
+    v, dir = carai.forward(a)
+    w, b = carai.get_weights()
+    car_ai3.assign_weights(w,b)
+    for (l1, l2) in zip(carai.fullyconnect_list, car_ai3.fullyconnect_list):
+        print(l1.weights == l2.weights)
+    carAIs = GeneOptimize.gene_algo(carai, 10)
+    for c in carAIs:
+        print(c == carai)
+
+
+
